b7062636f91adcc88222aea4c194ee444f8e2f23
ShobhitWK <shobhit@webkorps.com>
2022-07-27 16:11:57 +0530

    Added a while loop and a validator + link opener

--- spreadsheet.rb
+++ spreadsheet.rb
@@ -3,6 +3,7 @@
  Created By Shobhit Jain
 =end
 
+
 require 'date'
 require "bundler"
 Bundler.require
@@ -13,10 +14,19 @@ session = GoogleDrive::Session.from_service_account_key("client_secret.json")
 spreadsheet = session.spreadsheet_by_title("dailyreport")
 worksheet = spreadsheet.worksheets.first
 
-system("clear")
+START = "-----------------------------------------Daily Status Generator-----------------------------------------"
 
-print "How many rows you want to insert: "
-rows_no = gets.chomp.to_i
+while true
+  system("clear")
+  puts START
+  print "How many rows you want to insert: "
+  rows_no = gets.chomp.to_i
+  if rows_no.is_a?(Integer) && rows_no.to_i > 0
+    break
+  else
+    next
+  end
+end
 
 worksheet.delete_rows(2,rows_no+1)
 
@@ -54,4 +64,7 @@ print "\nChanges has been made!\n\n"
 
 worksheet.rows.first(10).each { |row| puts row.first(6).join(" | ")}
 
-puts "\nAccess the document from here: https://docs.google.com/spreadsheets/d/1j8OI826uuJN7VuOiiu3eJbsMo8UIGM5IJaS_wUjJhek"
+puts "\nOpening daily report in your browser."
+system("xdg-open", "https://docs.google.com/spreadsheets/d/1j8OI826uuJN7VuOiiu3eJbsMo8UIGM5IJaS_wUjJhek")
+
+puts "-"*START.length
